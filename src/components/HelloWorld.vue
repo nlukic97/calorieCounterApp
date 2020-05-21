7d4a73ce85d7b289b13d5c660558a9c8383a4ab2
nlukic97 <nlukic97@gmail.com>
2020-05-21 23:17:23 +0200

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,124 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(item, index) in listOfItems" v-bind:key="index">
+        <span>{{item.name}} |</span> <!--ovo v-bind nam treba ako vucemo neki data iz vue-a. Koji god attribut, ovo ce raditi -->
+        <span>{{item.calories}} |</span>
+        <span>{{item.carbs}} |</span>
+        <span>{{item.fat}} |</span>
+        <span>{{item.prot}} |</span>
+        &nbsp; <!-- no break space entity. Pravi mali razmak. -->
+        <button v-on:click="removeItem(index)">X</button> <!--prosledjuje se index koji je kao gore, kao parametar -->
+      </li> <!--Loop za hvatanje informacija. item je element, index je broj -->
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>
+      <span>Total:</span>
+      <span>{{totals.totalCal}} |</span>
+      <span>{{totals.totalCarbs}} |</span>
+      <span>{{totals.totalFat}} |</span>
+      <span>{{totals.totalProt}} |</span>
+    </div>
+    <div>
+      <input v-bind:placeholder="placeholderValues.inputNameText" v-model="newValues.newName" type="text">
+      <input v-bind:placeholder="placeholderValues.inputCalorieText" v-model="newValues.newCalories" type="number">
+      <input v-bind:placeholder="placeholderValues.inputCarbsText" v-model="newValues.newCarbs" type="number">
+      <input v-bind:placeholder="placeholderValues.inputFatText" v-model="newValues.newFat" type="number">
+      <input v-bind:placeholder="placeholderValues.inputProteinText" v-model="newValues.newProtein" type="number">
+      <button v-on:click="addItem">+</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: function(){
+    return {
+      placeholderValues:{
+        inputNameText:'Ide gas',
+        inputCalorieText:'',
+        inputCarbsText:'',
+        inputFatText:'',
+        inputProteinText:''
+        },
+      newValues:{
+        newName:'',
+        newCalories:'',
+        newCarbs:'',
+        newFat:'',
+        newProtein:''
+      },
+      listOfItems: [
+          {
+          name:'Sargarepa',
+          calories: 1000,
+          carbs: 500,
+          fat: 10,
+          prot: 100
+        },
+        {
+          name:'Krompir',
+          calories: 2000,
+          carbs: 800,
+          fat: 500,
+          prot: 10
+        },
+        {
+          name:'Dinja',
+          calories: 100,
+          carbs: 200,
+          fat: 210,
+          prot: 76
+        }
+      ],
+      totals: {
+        totalCal:0,
+        totalCarbs: 0,
+        totalFat: 0,
+        totalProt:0
+      }
+    }
+  },
+  methods: {
+    addItem: function(){
+      this.listOfItems.push({
+        name: this.newValues.newName,
+        calories: this.newValues.newCalories,
+        carbs: this.newValues.newCarbs,
+        fat: this.newValues.newFat,
+        prot: this.newValues.newProtein,
+      });
+      this.newValues.newName = null,      // promenio si ove u null jer kad je string zezne celu stvar. Kad je 0, vidi se.
+      this.newValues.newCalories = null,  
+      this.newValues.newCarbs = null,
+      this.newValues.newFat = null,
+      this.newValues.newProtein = null,
+      this.calcValues()
+    },
+    removeItem: function(itemIndex){ //itemIndex gleda index
+    this.listOfItems.splice(itemIndex,1); //ukloni element iz arraya koji ima ovaj index(itemIndex)
+    this.calcValues()
+    },
+    calcValues: function(){
+      //reset everything, so that it doesnt add up the existing add-up, double. So add all form start over.
+        this.totals.totalCal = 0;
+        this.totals.totalCarbs = 0;
+        this.totals.totalFat = 0;
+        this.totals.totalProt = 0;
+      for(let i = 0; this.listOfItems.length; i++){ //kad zakomentarisem cetiri linije koda ispod, nastane beskonacan loop. Zasto?
+        // console.log(this.totals.totalCal);
+        this.totals.totalCal += parseInt(this.listOfItems[i].calories);
+        this.totals.totalCarbs += parseInt(this.listOfItems[i].carbs);
+        this.totals.totalFat += parseInt(this.listOfItems[i].fat);
+        this.totals.totalProt += parseInt(this.listOfItems[i].prot);
+      }
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang='scss'>
+
 </style>
+
